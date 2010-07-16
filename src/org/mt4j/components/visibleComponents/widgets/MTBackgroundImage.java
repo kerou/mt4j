353332e486fe4b37715c99556f0c8d42908ac898
therion@gmx.net <therion@gmx.net@ce462a9e-2350-11df-aa88-49b14aa33a48>
2010-07-16 15:37:59 +0000

    * fix tiling in MTBackgroundImage when using non power of two texture sizes, but not recommended

--- src/org/mt4j/components/visibleComponents/widgets/MTBackgroundImage.java
+++ src/org/mt4j/components/visibleComponents/widgets/MTBackgroundImage.java
@@ -86,45 +86,25 @@ public class MTBackgroundImage extends MTPolygon {
 		
 		
 		if (MT4jSettings.getInstance().isOpenGlMode()){
-//			GLTextureParameters tp = new GLTextureParameters();
-//			tp.minFilter = GLTextureParameters.LINEAR; //"LINEAR" 
-//	//		tp.target = GLConstants.RECTANGULAR;
-////			GLTexture tex = new GLTexture(mtApp, mtApp.width, mtApp.height, tp);
-////			tex.putImage(bgImage);
-//			GLTexture tex = new GLTexture(mtApp, bgImage, tp);
-			
 			GLTextureSettings g = new GLTextureSettings(TEXTURE_TARGET.TEXTURE_2D, SHRINKAGE_FILTER.BilinearNoMipMaps, EXPANSION_FILTER.Bilinear, WRAP_MODE.REPEAT, WRAP_MODE.REPEAT); 
 			GLTexture tex;
 			if (pot){
 				tex = new GLTexture(mtApp, bgImage, g);
 			}else{
-				g.target = TEXTURE_TARGET.RECTANGULAR;
-				tex = new GLTexture(mtApp, bgImage, g);
+				if (tiled){
+					g.target = TEXTURE_TARGET.RECTANGULAR;
+					g.shrinkFilter = SHRINKAGE_FILTER.Trilinear; //Because NPOT texture with GL_REPEAT isnt supported -> gluBuild2Dmipmapds strechtes the texture to POT size
+					tex = new GLTexture(mtApp, bgImage, g);
+				}else{
+					g.target = TEXTURE_TARGET.RECTANGULAR;
+					tex = new GLTexture(mtApp, bgImage, g);
+				}
 			}
 			this.setTexture(tex);
 		}else{
 			this.setTexture(bgImage);
 		}
 		
-//		if (tiled){
-//			//Generate texture coordinates to repeat the texture over the whole background (works only with OpenGL)
-//			float u = (float)mtApp.width/(float)bgImage.width;
-//			float v = (float)mtApp.height/(float)bgImage.height;
-//			
-//			Vertex[] backgroundVertices = this.getVerticesLocal();
-//			backgroundVertices[0].setTexCoordU(0);
-//			backgroundVertices[0].setTexCoordV(0);
-//			backgroundVertices[1].setTexCoordU(u);
-//			backgroundVertices[1].setTexCoordV(0);
-//			backgroundVertices[2].setTexCoordU(u);
-//			backgroundVertices[2].setTexCoordV(v);
-//			backgroundVertices[3].setTexCoordU(0);
-//			backgroundVertices[3].setTexCoordV(v);
-//			
-//			//Update changed texture coordinates for opengl buffer drawing
-//			if (MT4jSettings.getInstance().isOpenGlMode())
-//				this.getGeometryInfo().updateTextureBuffer(this.isUseVBOs());
-//		}
 		this.setNoStroke(true);
 		this.setPickable(false);
 	}
