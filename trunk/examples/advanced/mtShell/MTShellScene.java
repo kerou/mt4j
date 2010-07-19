c1018e5b90339d133718e584910f2f28f59452c6
therion@gmx.net <therion@gmx.net@ce462a9e-2350-11df-aa88-49b14aa33a48>
2010-07-19 17:58:37 +0000

    * removed Eureka90.vlw font -> added Ziggurat.vlw instead (smaller file size) * fixed BitmapFontFactoryProxy to honor the default_font_path setting from MT4jSettings instead of only loading from /data path. * added LGPL License file for MRI3ds library * minor changes

--- trunk/examples/advanced/mtShell/MTShellScene.java
+++ trunk/examples/advanced/mtShell/MTShellScene.java
@@ -203,7 +203,7 @@ public class MTShellScene extends AbstractScene {
 		 */
 		
 		//TODO does the font exist on all platforms? fallback to arial.ttf?
-		font = FontManager.getInstance().createFont(app, "Comic Sans MS", 18, new MTColor(255,255,255), new MTColor(255,255,255));
+		font = FontManager.getInstance().createFont(app, "SansSerif", 18, new MTColor(255,255,255), new MTColor(255,255,255));
 //		font = FontManager.getInstance().createFont(app, "arial.ttf", 18, new MTColor(255,255,255), new MTColor(255,255,255));
 		
 		
