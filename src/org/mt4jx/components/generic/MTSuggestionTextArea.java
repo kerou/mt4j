0979dd7309d6218cee128eb6ba0edad596f826bb
MaginMichael <MaginMichael@ce462a9e-2350-11df-aa88-49b14aa33a48>
2010-08-09 00:03:35 +0000

    Added constructors to create the MTSuggestionTextArea with a specified font and/or with/without suggestions

--- src/org/mt4jx/components/generic/MTSuggestionTextArea.java
+++ src/org/mt4jx/components/generic/MTSuggestionTextArea.java
@@ -1,7 +1,6 @@
 package org.mt4jx.components.generic;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.mt4j.MTApplication;
@@ -9,6 +8,7 @@ import org.mt4j.components.StateChange;
 import org.mt4j.components.StateChangeEvent;
 import org.mt4j.components.StateChangeListener;
 import org.mt4j.components.TransformSpace;
+import org.mt4j.components.visibleComponents.font.IFont;
 import org.mt4j.components.visibleComponents.shapes.MTPolygon;
 import org.mt4j.components.visibleComponents.shapes.MTRectangle;
 import org.mt4j.components.visibleComponents.widgets.MTTextArea;
@@ -25,77 +25,233 @@ import processing.core.PGraphics;
 /**
  * The Class MTSuggestionTextArea.
  */
-public class MTSuggestionTextArea extends MTTextArea{
-	
+public class MTSuggestionTextArea extends MTTextArea {
+
+	/**
+	 * The listener interface for receiving edit events. The class that is
+	 * interested in processing a edit event implements this interface, and the
+	 * object created with that class is registered with a component using the
+	 * component's <code>addEditListener<code> method. When
+	 * the edit event occurs, that object's appropriate
+	 * method is invoked.
+	 * 
+	 * @see EditEvent
+	 */
+	public class EditListener implements IGestureEventListener {
+
+		/** The ta. */
+		MTTextArea ta;
+
+		/**
+		 * Instantiates a new edits the listener.
+		 * 
+		 * @param ta
+		 *            the ta
+		 */
+		public EditListener(MTTextArea ta) {
+			this.ta = ta;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.mt4j.input.inputProcessors.IGestureEventListener#processGestureEvent
+		 * (org.mt4j.input.inputProcessors.MTGestureEvent)
+		 */
+		@Override
+		public boolean processGestureEvent(MTGestureEvent ge) {
+			if (ge instanceof TapEvent) {
+				TapEvent te = (TapEvent) ge;
+				if (keyboard == null
+						&& te.getTapID() == TapEvent.BUTTON_CLICKED) {
+					keyboard = new MTKeyboard(app);
+					addChild(keyboard);
+					keyboard.addTextInputListener(ta);
+					keyboard.addStateChangeListener(
+							StateChange.COMPONENT_DESTROYED,
+							new KeyboardListener());
+					keyboard.setPositionRelativeToParent(calcPos(
+							ta,
+							keyboard,
+							0,
+							ta.getHeightXY(TransformSpace.LOCAL)
+									+ suggestionBox
+											.getHeightXY(TransformSpace.LOCAL)));
+				}
+			}
+			return false;
+		}
+
+	}
+
+	/**
+	 * The listener interface for receiving keyboard events. The class that is
+	 * interested in processing a keyboard event implements this interface, and
+	 * the object created with that class is registered with a component using
+	 * the component's <code>addKeyboardListener<code> method. When
+	 * the keyboard event occurs, that object's appropriate
+	 * method is invoked.
+	 * 
+	 * @see KeyboardEvent
+	 */
+	public class KeyboardListener implements StateChangeListener {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.mt4j.components.StateChangeListener#stateChanged(org.mt4j.components
+		 * .StateChangeEvent)
+		 */
+		@Override
+		public void stateChanged(StateChangeEvent evt) {
+			if (evt.getState() == StateChange.COMPONENT_DESTROYED) {
+				keyboard = null;
+			}
+
+		}
+
+	}
+
+	/**
+	 * The listener interface for receiving suggestionBox events. The class that
+	 * is interested in processing a suggestionBox event implements this
+	 * interface, and the object created with that class is registered with a
+	 * component using the component's
+	 * <code>addSuggestionBoxListener<code> method. When
+	 * the suggestionBox event occurs, that object's appropriate
+	 * method is invoked.
+	 * 
+	 * @see SuggestionBoxEvent
+	 */
+	public class SuggestionBoxListener implements IGestureEventListener {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.mt4j.input.inputProcessors.IGestureEventListener#processGestureEvent
+		 * (org.mt4j.input.inputProcessors.MTGestureEvent)
+		 */
+		@Override
+		public boolean processGestureEvent(MTGestureEvent ge) {
+			if (ge instanceof TapEvent) {
+				TapEvent te = (TapEvent) ge;
+				if (te.getTapID() == TapEvent.BUTTON_CLICKED) {
+					Vector3D w = Tools3D.project(app, app.getCurrentScene()
+							.getSceneCam(), te.getLocationOnScreen());
+					Vector3D x = suggestionBox.globalToLocal(w);
+					float zero = suggestionBox.getVerticesLocal()[0].y;
+					float heightPerLine = suggestionBox
+							.getHeightXY(TransformSpace.LOCAL)
+							/ (float) (suggestionBox.getLineCount() + 1);
+					int line = (int) ((x.y - zero) / heightPerLine);
+					System.out.println(line + " Line Count: "
+							+ suggestionBox.getLineCount()
+							+ " Height per Line: " + heightPerLine);
+					if (currentSuggestions.size() > line) {
+						setText(currentSuggestions.get(line));
+					}
+
+				}
+			}
+			return false;
+		}
+
+	}
+
 	/** The available suggestions. */
 	List<String> availableValues;
-	
+
 	/** The MTApplication. */
 	MTApplication app;
-	
+
 	/** The suggestion box. */
 	MTTextArea suggestionBox;
-	
+
 	/** The keyboard. */
 	MTKeyboard keyboard;
-	
+
 	/** The width. */
 	float width;
-	
+
 	/** The current suggestions. */
 	List<String> currentSuggestions = new ArrayList<String>();
-	
-	/**
-	 * Instantiates a new MTSuggestionTextArea
-	 *
-	 * @param app the MTApplication
-	 * @param width the width of the text input box
-	 * @param suggestions the suggestions as List<String>
-	 */
-	public MTSuggestionTextArea(MTApplication app, float width, List<String> suggestions) {
-		//Instantiate with default font, can be changed using CSS
-		super(app);
-		this.width = width;
-		this.setWidthLocal(width);
-		this.availableValues = suggestions;
-		this.app = app;
-		
-		//Add Tap listener to evoke Keyboard
-		this.setGestureAllowance(TapProcessor.class, true);
-		this.registerInputProcessor(new TapProcessor(app));
-		this.addGestureListener(TapProcessor.class, new EditListener(this));
-		
-		//Create Suggestion Box
-		suggestionBox = new MTTextArea(app, this.getFont());
-		suggestionBox.setWidthLocal(width);
-		suggestionBox.setCssForceDisable(true);
-		styleSuggestionBox();
-		
-		suggestionBox.removeAllGestureEventListeners();
-		suggestionBox.setGestureAllowance(TapProcessor.class, true);
-		suggestionBox.registerInputProcessor(new TapProcessor(app));
-		suggestionBox.addGestureListener(TapProcessor.class, new SuggestionBoxListener());
-		
-		this.addChild(suggestionBox);
-		drawSuggestionBox();
-	}
-	
-	public void styleSuggestionBox() {
-		suggestionBox.setNoFill(this.isNoFill());
-		suggestionBox.setNoStroke(this.isNoStroke());
-		suggestionBox.setFillColor(this.getFillColor());
-		suggestionBox.setStrokeColor(new MTColor(this.getStrokeColor().getR(), this.getStrokeColor().getG(),this.getStrokeColor().getG(),this.getStrokeColor().getAlpha() * 0.75f));
-		suggestionBox.setStrokeWeight(0.25f);
-	}
-	
+
 	/** The o (Counting Variable). */
 	int o = 0;
-	
+
 	/** The p (Counting Variable). */
 	int p = 0;
-	
-	/* (non-Javadoc)
-	 * @see org.mt4j.components.visibleComponents.widgets.MTTextArea#drawComponent(processing.core.PGraphics)
+
+	/**
+	 * Instantiates a new MTSuggestionTextArea.
+	 * 
+	 * @param app
+	 *            the MTApplication
+	 * @param width
+	 *            the width
+	 */
+	public MTSuggestionTextArea(MTApplication app, float width) {
+		this(app, width, new ArrayList<String>());
+	}
+
+	/**
+	 * Instantiates a new MTSuggestionTextArea.
+	 * 
+	 * @param app
+	 *            the MTApplication
+	 * @param width
+	 *            the width of the text input box
+	 * @param suggestions
+	 *            the suggestions as List<String>
+	 */
+	public MTSuggestionTextArea(MTApplication app, float width,
+			List<String> suggestions) {
+		// Instantiate with default font, can be changed using CSS
+		super(app);
+		this.init(app, width, suggestions);
+	}
+
+	/**
+	 * Instantiates a new MTSuggestionTextArea.
+	 * 
+	 * @param app
+	 *            the MTApplication
+	 * @param font
+	 *            the font
+	 * @param width
+	 *            the width of the text input box
+	 */
+	public MTSuggestionTextArea(MTApplication app, IFont font, float width) {
+		this(app, font, width, new ArrayList<String>());
+	}
+
+	/**
+	 * Instantiates a new MTSuggestionTextArea.
+	 * 
+	 * @param app
+	 *            the MTApplication
+	 * @param font
+	 *            the font
+	 * @param width
+	 *            the width of the text input box
+	 * @param suggestions
+	 *            the suggestions as List<String>
+	 */
+	public MTSuggestionTextArea(MTApplication app, IFont font, float width,
+			List<String> suggestions) {
+		super(app, font);
+		this.init(app, width, suggestions);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.mt4j.components.visibleComponents.widgets.MTTextArea#drawComponent
+	 * (processing.core.PGraphics)
 	 */
 	@Override
 	public void drawComponent(PGraphics g) {
@@ -104,74 +260,28 @@ public class MTSuggestionTextArea extends MTTextArea{
 			if (o++ > 30) {
 				o = 0;
 				drawSuggestionBox();
-				
+
 			}
 			if (p++ > 4) {
 				this.setWidthLocal(width);
 			}
-		}
-		
-	}
-	
-	/**
-	 * Draw suggestion box.
-	 */
-	private void drawSuggestionBox() {
-		
-		String suggestions = "";
-		int i = 0;
-		List<String> strings = this.getRelevantStrings();
-		if (strings.size() > 0) {
-			suggestionBox.setVisible(true);
-		currentSuggestions.clear();
-		for (String s: strings) {
-			if (i != 0 && i < 5) suggestions += "\n";
-			
-			if (i++ < 5) {
-				suggestions += s;
-				currentSuggestions.add(s);
-			} else {
-				break;
-			}
-			
-		}
-		suggestionBox.setText(suggestions);
-		suggestionBox.setWidthLocal(width);
-		suggestionBox.setPositionRelativeToParent(calcPos(this, suggestionBox, 0, this.getHeightXY(TransformSpace.LOCAL)));
 		} else {
-			suggestionBox.setVisible(false);
+			if (suggestionBox.isVisible() == true)
+				suggestionBox.setVisible(false);
 		}
-		if (keyboard == null) suggestionBox.setVisible(false);
-	}
-	
-	/**
-	 * Calculate the Coordinates needed for placing the Rectangle
-	 *
-	 * @param rect the Rectangle
-	 * @param ta the TextArea
-	 * @param xo the x-offset
-	 * @param yo the y-offset
-	 * @return the position as Vector3D
-	 */
-	private Vector3D calcPos(MTRectangle rect, MTPolygon ta, float xo, float yo) {
 
-		return new Vector3D((ta.getWidthXYVectLocal().length() / 2)
-				+ rect.getVerticesLocal()[0].getX() + xo, (ta
-				.getHeightXYVectLocal().length() / 2)
-				+ rect.getVerticesLocal()[0].getY() + yo);
 	}
-	
-	
+
 	/**
-	 * Gets the relevant strings (for the current content of the TextArea)
-	 *
+	 * Gets the relevant strings (for the current content of the TextArea).
+	 * 
 	 * @return the relevant strings
 	 */
 	public List<String> getRelevantStrings() {
 		List<String> newList = new ArrayList<String>();
 		if (!availableValues.isEmpty()) {
-		String currentText = this.getText();
-			for (String s: availableValues) {
+			String currentText = this.getText();
+			for (String s : availableValues) {
 				if (currentText != "") {
 					if (s.toUpperCase().contains(currentText.toUpperCase())) {
 						newList.add(s.replaceAll("\n", " "));
@@ -183,114 +293,109 @@ public class MTSuggestionTextArea extends MTTextArea{
 		}
 		return newList;
 	}
-	
 
 	/**
-	 * The listener interface for receiving edit events.
-	 * The class that is interested in processing a edit
-	 * event implements this interface, and the object created
-	 * with that class is registered with a component using the
-	 * component's <code>addEditListener<code> method. When
-	 * the edit event occurs, that object's appropriate
-	 * method is invoked.
-	 *
-	 * @see EditEvent
+	 * Style suggestion box.
 	 */
-	public class EditListener implements IGestureEventListener {
-
-		/** The ta. */
-		MTTextArea ta;
-		
-		/**
-		 * Instantiates a new edits the listener.
-		 *
-		 * @param ta the ta
-		 */
-		public EditListener(MTTextArea ta) {
-			this.ta = ta;
-		}
-		
-		/* (non-Javadoc)
-		 * @see org.mt4j.input.inputProcessors.IGestureEventListener#processGestureEvent(org.mt4j.input.inputProcessors.MTGestureEvent)
-		 */
-		@Override
-		public boolean processGestureEvent(MTGestureEvent ge) {
-			if (ge instanceof TapEvent) {
-				TapEvent te = (TapEvent)ge;
-				if (keyboard == null && te.getTapID() == TapEvent.BUTTON_CLICKED) {
-					keyboard = new MTKeyboard(app);
-					addChild(keyboard);
-					keyboard.addTextInputListener(ta);
-					keyboard.addStateChangeListener(StateChange.COMPONENT_DESTROYED, new KeyboardListener());
-					keyboard.setPositionRelativeToParent(calcPos(ta, keyboard, 0, ta.getHeightXY(TransformSpace.LOCAL) + suggestionBox.getHeightXY(TransformSpace.LOCAL)));
-				}
-			}
-			return false;
-		}
-		
+	public void styleSuggestionBox() {
+		suggestionBox.setNoFill(this.isNoFill());
+		suggestionBox.setNoStroke(this.isNoStroke());
+		suggestionBox.setFillColor(this.getFillColor());
+		suggestionBox.setStrokeColor(new MTColor(this.getStrokeColor().getR(),
+				this.getStrokeColor().getG(), this.getStrokeColor().getG(),
+				this.getStrokeColor().getAlpha() * 0.75f));
+		suggestionBox.setStrokeWeight(0.25f);
 	}
-	
-	/**
-	 * The listener interface for receiving keyboard events.
-	 * The class that is interested in processing a keyboard
-	 * event implements this interface, and the object created
-	 * with that class is registered with a component using the
-	 * component's <code>addKeyboardListener<code> method. When
-	 * the keyboard event occurs, that object's appropriate
-	 * method is invoked.
-	 *
-	 * @see KeyboardEvent
-	 */
-	public class KeyboardListener implements StateChangeListener {
 
-		/* (non-Javadoc)
-		 * @see org.mt4j.components.StateChangeListener#stateChanged(org.mt4j.components.StateChangeEvent)
-		 */
-		@Override
-		public void stateChanged(StateChangeEvent evt) {
-			if (evt.getState() == StateChange.COMPONENT_DESTROYED) {
-				keyboard = null;
-			}
-			
-		}
-		
+	/**
+	 * Calculate the Coordinates needed for placing the Rectangle.
+	 * 
+	 * @param rect
+	 *            the Rectangle
+	 * @param ta
+	 *            the TextArea
+	 * @param xo
+	 *            the x-offset
+	 * @param yo
+	 *            the y-offset
+	 * @return the position as Vector3D
+	 */
+	private Vector3D calcPos(MTRectangle rect, MTPolygon ta, float xo, float yo) {
+
+		return new Vector3D((ta.getWidthXYVectLocal().length() / 2)
+				+ rect.getVerticesLocal()[0].getX() + xo, (ta
+				.getHeightXYVectLocal().length() / 2)
+				+ rect.getVerticesLocal()[0].getY() + yo);
 	}
-	
-	/**
-	 * The listener interface for receiving suggestionBox events.
-	 * The class that is interested in processing a suggestionBox
-	 * event implements this interface, and the object created
-	 * with that class is registered with a component using the
-	 * component's <code>addSuggestionBoxListener<code> method. When
-	 * the suggestionBox event occurs, that object's appropriate
-	 * method is invoked.
-	 *
-	 * @see SuggestionBoxEvent
-	 */
-	public class SuggestionBoxListener implements IGestureEventListener {
 
-		/* (non-Javadoc)
-		 * @see org.mt4j.input.inputProcessors.IGestureEventListener#processGestureEvent(org.mt4j.input.inputProcessors.MTGestureEvent)
-		 */
-		@Override
-		public boolean processGestureEvent(MTGestureEvent ge) {
-			if (ge instanceof TapEvent) {
-				TapEvent te = (TapEvent)ge;
-				if (te.getTapID() == TapEvent.BUTTON_CLICKED) {
-					Vector3D w = Tools3D.project(app, app.getCurrentScene().getSceneCam(), te.getLocationOnScreen());
-					Vector3D x = suggestionBox.globalToLocal(w);
-					float zero = suggestionBox.getVerticesLocal()[0].y;
-					float heightPerLine = suggestionBox.getHeightXY(TransformSpace.LOCAL) / (float)(suggestionBox.getLineCount() + 1);
-					int line = (int)((x.y - zero) / heightPerLine);
-					System.out.println(line + " Line Count: " + suggestionBox.getLineCount() + " Height per Line: " + heightPerLine);
-					if (currentSuggestions.size() > line) {
-						setText(currentSuggestions.get(line));
-					}
-					
+	/**
+	 * Draw suggestion box.
+	 */
+	private void drawSuggestionBox() {
+
+		String suggestions = "";
+		int i = 0;
+		List<String> strings = this.getRelevantStrings();
+		if (strings.size() > 0) {
+			suggestionBox.setVisible(true);
+			currentSuggestions.clear();
+			for (String s : strings) {
+				if (i != 0 && i < 5)
+					suggestions += "\n";
+
+				if (i++ < 5) {
+					suggestions += s;
+					currentSuggestions.add(s);
+				} else {
+					break;
 				}
+
 			}
-			return false;
+			suggestionBox.setText(suggestions);
+			suggestionBox.setWidthLocal(width);
+			suggestionBox.setPositionRelativeToParent(calcPos(this,
+					suggestionBox, 0, this.getHeightXY(TransformSpace.LOCAL)));
+		} else {
+			suggestionBox.setVisible(false);
 		}
-		
+		if (keyboard == null)
+			suggestionBox.setVisible(false);
+	}
+
+	/**
+	 * Inits the MTSuggestionTextArea
+	 * 
+	 * @param app
+	 *            the app
+	 * @param width
+	 *            the width
+	 * @param suggestions
+	 *            the suggestions as List<String>
+	 */
+	private void init(MTApplication app, float width, List<String> suggestions) {
+		this.width = width;
+		this.setWidthLocal(width);
+		this.availableValues = suggestions;
+		this.app = app;
+
+		// Add Tap listener to evoke Keyboard
+		this.setGestureAllowance(TapProcessor.class, true);
+		this.registerInputProcessor(new TapProcessor(app));
+		this.addGestureListener(TapProcessor.class, new EditListener(this));
+
+		// Create Suggestion Box
+		suggestionBox = new MTTextArea(app, this.getFont());
+		suggestionBox.setWidthLocal(width);
+		suggestionBox.setCssForceDisable(true);
+		styleSuggestionBox();
+
+		suggestionBox.removeAllGestureEventListeners();
+		suggestionBox.setGestureAllowance(TapProcessor.class, true);
+		suggestionBox.registerInputProcessor(new TapProcessor(app));
+		suggestionBox.addGestureListener(TapProcessor.class,
+				new SuggestionBoxListener());
+
+		this.addChild(suggestionBox);
+		drawSuggestionBox();
 	}
 }
