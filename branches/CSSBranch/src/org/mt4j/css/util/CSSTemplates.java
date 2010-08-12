6a0ad4521137f7b57a7513688383af6b25643c3b
MaginMichael <MaginMichael@ce462a9e-2350-11df-aa88-49b14aa33a48>
2010-08-12 15:02:29 +0000

    Added Templates for styling objects

---
+++ branches/CSSBranch/src/org/mt4j/css/util/CSSTemplates.java
@@ -0,0 +1,15 @@
+package org.mt4j.css.util;
+
+public class CSSTemplates {
+	
+	public static String REDSTYLE = "templates/redstyle.css";
+	public static String MATRIXSTYLE = "templates/matrixstyle.css";
+	public static String WHITESTYLE = "templates/whitestyle.css";
+	public static String GREENSTYLE = "templates/greenstyle.css";
+	
+	
+
+	
+	
+	
+}
