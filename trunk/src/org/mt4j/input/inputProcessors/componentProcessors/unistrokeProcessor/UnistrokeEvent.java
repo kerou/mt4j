7988705427bba715d2e4ef29d47986286701eff2
therion@gmx.net <therion@gmx.net@ce462a9e-2350-11df-aa88-49b14aa33a48>
2010-09-09 19:21:34 +0000

    * removed that MTCamera implements IFrustum * added stroke gestures to MTGesturesExamplesScene * minor changes

--- trunk/src/org/mt4j/input/inputProcessors/componentProcessors/unistrokeProcessor/UnistrokeEvent.java
+++ trunk/src/org/mt4j/input/inputProcessors/componentProcessors/unistrokeProcessor/UnistrokeEvent.java
@@ -5,13 +5,13 @@ import org.mt4j.components.interfaces.IMTComponent3D;
 import org.mt4j.components.visibleComponents.shapes.MTPolygon;
 import org.mt4j.input.inputProcessors.IInputProcessor;
 import org.mt4j.input.inputProcessors.MTGestureEvent;
-import org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeUtils.DollarGesture;
+import org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeUtils.UnistrokeGesture;
 
 public class UnistrokeEvent extends MTGestureEvent{
 	private MTPolygon visualization;
-	private DollarGesture gesture;
+	private UnistrokeGesture gesture;
 
-	public UnistrokeEvent(IInputProcessor source, int id, IMTComponent3D targetComponent, MTPolygon visualization, DollarGesture gesture) {
+	public UnistrokeEvent(IInputProcessor source, int id, IMTComponent3D targetComponent, MTPolygon visualization, UnistrokeGesture gesture) {
 		super(source, id, targetComponent);
 		this.visualization = visualization;
 		this.gesture = gesture;
@@ -21,7 +21,7 @@ public class UnistrokeEvent extends MTGestureEvent{
 		return this.visualization;
 	}
 	
-	public DollarGesture getGesture(){
+	public UnistrokeGesture getGesture(){
 		return this.gesture;
 	}
 	
