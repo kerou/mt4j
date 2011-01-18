719d6b87df84de3020b78c09bd3b10e3482374ba
therion@gmx.net <therion@gmx.net@ce462a9e-2350-11df-aa88-49b14aa33a48>
2011-01-18 15:58:34 +0000

    * added OpenGL abstraction classes

---
+++ src/org/mt4j/MTDesktopApplication.java
@@ -0,0 +1,21 @@
+package org.mt4j;
+
+import javax.media.opengl.GL;
+
+import processing.opengl.PGraphicsOpenGL;
+
+public abstract class MTDesktopApplication extends MTApplication {
+	
+//	public GL getGL(){
+//		return ((PGraphicsOpenGL)g).gl;
+//	}
+//	
+//	public GL beginGL(){
+//		return ((PGraphicsOpenGL)g).beginGL();
+//	}
+//	
+//	public void endGL(){
+//		((PGraphicsOpenGL)g).endGL();
+//	}
+
+}
