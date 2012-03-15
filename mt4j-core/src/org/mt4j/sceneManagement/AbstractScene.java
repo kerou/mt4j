24ae8965eadc075965afa4f7a218ccb2d2fa9711
therion@gmx.net <therion@gmx.net@ce462a9e-2350-11df-aa88-49b14aa33a48>
2012-03-15 15:38:19 +0000

    * fix stereo drawing made android apps crash due to glDrawBuffer() not available

--- mt4j-core/src/org/mt4j/sceneManagement/AbstractScene.java
+++ mt4j-core/src/org/mt4j/sceneManagement/AbstractScene.java
@@ -162,19 +162,22 @@ public abstract class AbstractScene implements Iscene {
 		}
 		
 		/*
-		 * Update and render canvas (for left eye if stereoscopic rendering is enabled)
+		 * Update and render canvas (for left eye if stereoscopic rendering is enabled
 		 */
-		if (MT4jSettings.getInstance().isOpenGlMode() && !PlatformUtil.isAndroid() && MT4jSettings.getInstance().isStereoscopic()){
-			GL10 gl = PlatformUtil.getGL();
-			gl.glDrawBuffer(GL10.GL_BACK_LEFT);
+		if (!PlatformUtil.isAndroid()){
+			if (MT4jSettings.getInstance().isOpenGlMode() &&  MT4jSettings.getInstance().isStereoscopic()){
+				GL10 gl = PlatformUtil.getGL();
+				gl.glDrawBuffer(GL10.GL_BACK_LEFT);
+			
+				// Locate cam for left eye
+				this.getSceneCam().moveCamAndViewCenter(-MT4jSettings.getInstance().getEyeSeparation() / 2.0f, 0, 0);
+			}
+			else {
+				GL10 gl = PlatformUtil.getGL();
+				gl.glDrawBuffer(GL10.GL_BACK);  //TODO do only once when switching off stereoscopic rendering, rather than each frame?
+			}
+		}
 		
-			// Locate cam for left eye
-			this.getSceneCam().moveCamAndViewCenter(-MT4jSettings.getInstance().getEyeSeparation() / 2.0f, 0, 0);
-		}
-		else {
-			GL10 gl = PlatformUtil.getGL();
-			gl.glDrawBuffer(GL10.GL_BACK);  //TODO do only once when switching off stereoscopic rendering, rather than each frame?
-		}
 		
 		//Clear the background
 		if (this.clearBeforeDraw){
@@ -185,23 +188,25 @@ public abstract class AbstractScene implements Iscene {
 		this.getCanvas().drawAndUpdateCanvas(graphics, timeDelta);
 		
 		 //Render canvas for right eye if stereoscopic rendering is enabled
-		if (MT4jSettings.getInstance().isOpenGlMode() && !PlatformUtil.isAndroid() && MT4jSettings.getInstance().isStereoscopic()){
-			GL10 gl = PlatformUtil.getGL();
-			gl.glDrawBuffer(GL10.GL_BACK_RIGHT);
-				
-			// Locate cam for right eye
-			this.getSceneCam().moveCamAndViewCenter(MT4jSettings.getInstance().getEyeSeparation(), 0, 0);
-			
-			// Clear the background
-			if (this.clearBeforeDraw){ 
-				this.clear(graphics);
+		if (!PlatformUtil.isAndroid()){
+			if (MT4jSettings.getInstance().isOpenGlMode()  && MT4jSettings.getInstance().isStereoscopic()){
+				GL10 gl = PlatformUtil.getGL();
+				gl.glDrawBuffer(GL10.GL_BACK_RIGHT);
+
+				// Locate cam for right eye
+				this.getSceneCam().moveCamAndViewCenter(MT4jSettings.getInstance().getEyeSeparation(), 0, 0);
+
+				// Clear the background
+				if (this.clearBeforeDraw){ 
+					this.clear(graphics);
+				}
+
+				// Draw canvas (w/o update)
+				this.getCanvas().redrawCanvas(graphics);
+
+				// Center cam
+				this.getSceneCam().moveCamAndViewCenter(-MT4jSettings.getInstance().getEyeSeparation() / 2.0f, 0, 0);
 			}
-			
-			// Draw canvas (w/o update)
-			this.getCanvas().redrawCanvas(graphics);
-			
-			// Center cam
-			this.getSceneCam().moveCamAndViewCenter(-MT4jSettings.getInstance().getEyeSeparation() / 2.0f, 0, 0);
 		}
 	}
 	
